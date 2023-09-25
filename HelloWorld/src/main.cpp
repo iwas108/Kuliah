932c9b41b54c5b994b2b92b80ec8e532e05e5d2c
iwas108 <64204327+iwas108@users.noreply.github.com>
2023-09-25 14:39:28 +0800

    test

--- HelloWorld/src/main.cpp
+++ HelloWorld/src/main.cpp
@@ -1,18 +1,12 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(){
+  pinMode(13, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop(){
+  digitalWrite(13, HIGH);
+  delay(250);
+  digitalWrite(13, LOW);
+  delay(250);
 }
