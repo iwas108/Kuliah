81c6b4e9dec760e27248bb17f843a298c6805b9d
iwas108 <64204327+iwas108@users.noreply.github.com>
2023-11-20 14:26:02 +0800

    first working WS

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ const char* pass = "#tiundiknas";
 
 WiFiMulti myWiFi;
 AsyncWebServer myWeb(80);
+AsyncWebSocket myWs("/ws");
 
 // put function declarations here:
 int myFunction(int, int);
@@ -19,6 +20,7 @@ int myFunction(int, int);
 void onWiFiConnected(WiFiEvent_t event, WiFiEventInfo_t info);
 void onWiFiDisconnected(WiFiEvent_t event, WiFiEventInfo_t info);
 void onWiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info);
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len);
 
 void setup() {
   // put your setup code here, to run once:
@@ -46,11 +48,27 @@ void setup() {
   }
 
   myWeb.begin();
+  myWs.onEvent(onWsEvent);
+  myWeb.addHandler(&myWs);
 }
 
+unsigned long SCHEDULER_WS_ROUTINE = 0;
 void loop() {
   // put your main code here, to run repeatedly:
   myWiFi.run();
+  myWs.cleanupClients();
+
+  //Eksekusi setiap 1 detik sekali
+  unsigned long now = millis();
+  if( (now - SCHEDULER_WS_ROUTINE) >= 1000 ){
+
+    //broadcast data ke semua klien
+    if(myWs.count() > 0){
+      myWs.textAll(String(min(max(2 * (WiFi.RSSI() + 100), 0), 100)).c_str());
+    }
+
+    SCHEDULER_WS_ROUTINE = now;
+  }
 }
 
 // put function definitions here:
@@ -61,7 +79,7 @@ int myFunction(int x, int y) {
 void onWiFiConnected(WiFiEvent_t event, WiFiEventInfo_t info){
   Serial.print("WiFi connected: ");
   Serial.print(WiFi.SSID());
-  Serial.print(" Signal: ");
+  Serial.print(" | Signal: ");
   Serial.print(min(max(2 * (WiFi.RSSI() + 100), 0), 100));
   Serial.println("%");
 }
@@ -73,4 +91,76 @@ void onWiFiDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
 void onWiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
   Serial.print("Got IP Address: ");
   Serial.println(WiFi.localIP());
+}
+
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+  if(type == WS_EVT_CONNECT){
+    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+    client->printf("Hello Client %u :)", client->id());
+    client->ping();
+  } else if(type == WS_EVT_DISCONNECT){
+    Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
+  } else if(type == WS_EVT_ERROR){
+    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+  } else if(type == WS_EVT_PONG){
+    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+  } else if(type == WS_EVT_DATA){
+    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+    String msg = "";
+    if(info->final && info->index == 0 && info->len == len){
+      //the whole message is in a single frame and we got all of it's data
+      Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+
+      if(info->opcode == WS_TEXT){
+        for(size_t i=0; i < info->len; i++) {
+          msg += (char) data[i];
+        }
+      } else {
+        char buff[3];
+        for(size_t i=0; i < info->len; i++) {
+          sprintf(buff, "%02x ", (uint8_t) data[i]);
+          msg += buff ;
+        }
+      }
+      Serial.printf("%s\n",msg.c_str());
+
+      if(info->opcode == WS_TEXT)
+        client->text("I got your text message");
+      else
+        client->binary("I got your binary message");
+    } else {
+      //message is comprised of multiple frames or the frame is split into multiple packets
+      if(info->index == 0){
+        if(info->num == 0)
+          Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+        Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+      }
+
+      Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
+
+      if(info->opcode == WS_TEXT){
+        for(size_t i=0; i < len; i++) {
+          msg += (char) data[i];
+        }
+      } else {
+        char buff[3];
+        for(size_t i=0; i < len; i++) {
+          sprintf(buff, "%02x ", (uint8_t) data[i]);
+          msg += buff ;
+        }
+      }
+      Serial.printf("%s\n",msg.c_str());
+
+      if((info->index + len) == info->len){
+        Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+        if(info->final){
+          Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+          if(info->message_opcode == WS_TEXT)
+            client->text("I got your text message");
+          else
+            client->binary("I got your binary message");
+        }
+      }
+    }
+  }
 }
